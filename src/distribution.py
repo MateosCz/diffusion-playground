96ee04a50bcbcec11d5b0970930facf641dac059
Jingchao Zhou <jizho@dtu.dk>
2026-04-21 09:12:18 +0200

    Add Pdf and score for wrapped normal distribution.

--- src/distribution.py
+++ src/distribution.py
@@ -21,10 +21,33 @@ class BaseDistribution(ABC,nn.Module):
     def sample_shape(self, shape: torch.Tensor):
         raise NotImplementedError
 
-class WrappedNormalDistribution(BaseDistribution):
-    def __init__(self, sigma, trunc_n):
+
+class WrappedNormalDistribution():
+    def __init__(self, mu, sigma, trunc_n):
+        self.mu = mu
         self.sigma = sigma
         self.trunc_n = trunc_n
+
+    # given the value x, return the score of the distribution p(x)
+    # because mu and sigma are t related, so the score is also t related
+    def score(self, x, T:float = 2 * torch.pi):
+        C_2 = 0
+        C = 0
+        for k in range(-self.trunc_n, self.trunc_n+1):
+            C_component = torch.exp(-(x - self.mu + k * T)**2)/(2*self.sigma**2)
+            C2_component = (x - self.mu + k *T) / self.sigma**2
+            C_2 += C_component * C2_component
+            C += C_component
+        
+        return C_2 / C
+            
+
+
+
+
+
+
+
 
     # def sample(self):
 
